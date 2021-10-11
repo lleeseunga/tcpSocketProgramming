4cbc60194d9c003230e219beee68f50d29860a1d
Selina <73357741+seungaaa@users.noreply.github.com>
2021-10-11 14:03:47 -0400

    files upload

---
+++ tcpClient.py
@@ -0,0 +1,25 @@
+import socket
+import sys
+
+host = socket.gethostname()
+port = 12345
+FORMAT = "utf-8"
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((host, port))
+    filename = sys.argv[1]
+    
+    with open(filename,"rb") as f:
+        print("Sending...")
+
+        l = f.read(1024)
+        s.send(filename.encode(FORMAT))
+        s.recv(1024).decode(FORMAT)
+
+        while (l):
+            #print("Sending...")
+            s.send(l)
+            s.recv(1024).decode(FORMAT)
+            l = f.read(1024)
+    print("Done Sending")
---
+++ tcpServer.py
@@ -0,0 +1,31 @@
+import socket               
+
+host = socket.gethostname() 
+port = 12345
+FORMAT = "utf-8"
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((host, port))
+    s.listen(5)
+    
+    while True:
+        c,addr = s.accept()
+        print("Got connection from", addr)
+
+        l = c.recv(1024)
+        arr = (l.decode(FORMAT)).split(".")
+        filename = arr[0] + "_received." + arr[1]
+        
+        with open(filename,"wb") as f:
+            c.send(filename.encode(FORMAT))
+            print("Receiving...")
+            while (l):
+                l = c.recv(1024)
+                f.write(l)
+                c.send(filename.encode(FORMAT))
+            print("Done Receiving")
+        c.close()
+
+
+
+
